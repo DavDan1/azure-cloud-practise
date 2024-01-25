ca4514e4b4831499a5bd657f1cc8eb885ad4d83a
Davit Danielyan <davit-danielyan@hotmail.com>
2024-01-25 16:53:25 +0100

    Add blob to program.cs

--- azure-practice-dav/Program.cs
+++ azure-practice-dav/Program.cs
@@ -2,24 +2,54 @@ using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Models;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Retrieve the Azure Storage connection string from the configuration
+string tableStorageUri = builder.Configuration["ConnectionStrings:blobdav"];
+
+if (string.IsNullOrEmpty(tableStorageUri))
+{
+    Console.WriteLine("Azure Storage connection string not found in configuration.");
+    // Handle the missing configuration appropriately, throw an exception, log an error, etc.
+    return;
+}
+
+builder.Services.AddSingleton<BlobService>();
+
+builder.Services.AddSingleton(sp => tableStorageUri);
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnectionString") ?? throw new InvalidOperationException("Connection string 'DbConnectionString' not found.")));
-//This line adds the CORS services to the container.
-builder.Services.AddCors(); //<-- Add this line to register CORS services
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnectionString") ?? throw new InvalidOperationException("Connection string 'DbConnectionString' not found.")), ServiceLifetime.Scoped);
+
+// This line adds the CORS services to the container.
+builder.Services.AddCors();
+
 builder.Services.AddControllers();
+
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
 });
+
 var app = builder.Build();
+
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
-    SeedData.Initialize(services);
+
+    // Add logging to help diagnose the issue
+    Console.WriteLine($"Retrieving tableStorageUri: {tableStorageUri}");
+
+    // Move the retrieval of retrievedTableStorageUri inside the using block
+    var retrievedTableStorageUri = services.GetRequiredService<string>();
+
+    SeedData.Initialize(services, tableStorageUri);
 }
+
 // Now you can configure the CORS policy.
 app.UseCors(policy =>
 {
@@ -27,11 +57,14 @@ app.UseCors(policy =>
           .AllowAnyMethod()
           .AllowAnyHeader();
 });
+
+
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
 });
+
 app.UseHttpsRedirection();
 app.MapControllers();
 app.Run();
