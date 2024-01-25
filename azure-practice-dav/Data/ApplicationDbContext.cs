cfb862a93daeaaec36f6f24254b757eafdfed3ca
Davit Danielyan <davit-danielyan@hotmail.com>
2024-01-25 16:54:26 +0100

    Add table uri

--- azure-practice-dav/Data/ApplicationDbContext.cs
+++ azure-practice-dav/Data/ApplicationDbContext.cs
@@ -5,12 +5,16 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Models;
 
-    public class ApplicationDbContext : DbContext
-    {
-        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+public class ApplicationDbContext : DbContext
+{
+    private readonly string _tableStorageUri;
 
-        public DbSet<Movie> Movies { get; set; } = default!;
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, string tableStorageUri)
+        : base(options)
+    {
+        _tableStorageUri = tableStorageUri;
+
     }
+
+    public DbSet<Movie> Movies { get; set; } = default!;
+}
